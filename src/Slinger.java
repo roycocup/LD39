b1137897c525f97cd4372b18f16cd9e4d944185f
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-07-29 11:58:42 +0100

    first blood

---
+++ src/Slinger.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class Slinger extends PApplet {
+
+	public void settings(){
+		size(800, 600);
+	}
+
+	public void setup(){
+		
+	}
+	
+	
+	public void draw(){
+		background(255,155,253); // purple
+	}
+	
+	
+	public static void main(String[] args) {
+		PApplet.main("Slinger");
+	}
+
+}
