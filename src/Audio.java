37e4519ad9381221c3504f00c8e32fbf88c454f7
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-07-30 10:37:46 +0100

    deleting build from repo

--- src/Audio.java
+++ src/Audio.java
@@ -1,5 +1,9 @@
 import java.io.File;
 import java.net.URL;
+
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
+import javax.media.EndOfMediaEvent;
 import javax.media.Manager;
 import javax.media.MediaLocator;
 import javax.media.Player;
@@ -9,6 +13,7 @@ import javax.sound.sampled.*;
 public class Audio {
 
 	Slinger g;
+	Player player; 
 	
 	public Audio(Slinger g){
 		this.g = g;
@@ -16,9 +21,17 @@ public class Audio {
 		try {
 			URL url = new File(g.inGameMusic).toURI().toURL();
 			MediaLocator ml = new MediaLocator(url);
-			Player player = Manager.createPlayer(ml);
+			player = Manager.createPlayer(ml);
 			player.realize();
 			player.start();
+			player.addControllerListener(new ControllerListener(){
+				public void controllerUpdate(ControllerEvent e){
+					if (e instanceof EndOfMediaEvent){
+						player.stop();
+						player.close();
+			         }
+				}
+			});
 			
 //			AudioInputStream audioIn = AudioSystem.getAudioInputStream(g.inGameMusic);
 //	        Clip clip = AudioSystem.getClip();
